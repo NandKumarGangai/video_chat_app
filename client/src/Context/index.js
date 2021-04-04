da4b982fea85624bc08d4b1cb3c3c42aa21c3ed9
nandkumargangai1@outlook.com <nandkumargangai1@outlook.com>
2021-04-04 12:41:22 +0530

    Updated scripts.

--- client/src/Context/index.js
+++ client/src/Context/index.js
@@ -4,7 +4,8 @@ import Peer from 'simple-peer';
 
 export const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+// const socket = io('http://localhost:5000');
+const socket = io('https://video-chat-app-ng.herokuapp.com/');
 
 const ContextProvider = ({ children }) => {
     const [stream, setStream] = useState(null);
